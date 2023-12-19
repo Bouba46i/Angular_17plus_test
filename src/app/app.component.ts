a25417f5986ec9ec8221314d7a92aa33eac64ac2
Bastian Bouvy <bastian.bouvy@gmail.com>
2023-12-19 23:18:58 +0100

    ➕ button, form & form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './button/button.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet, ButtonComponent],
+	templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'TestV17';
+	title = 'TestV17';
 
-  // users = await fetch('https://api.publicapis.org/').then() ...
+	// users = await fetch('https://api.publicapis.org/').then() ...
 }
